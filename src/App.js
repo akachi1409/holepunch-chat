4a592c2be5a62c6a9387be8fc59149ff8f978554
Lucas Barrena <lucasbarrena98@gmail.com>
2023-01-26 15:56:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 import "./App.css";
 import b4a from "b4a";
 import DHT from "@hyperswarm/dht-relay";
@@ -13,33 +13,65 @@ import Hyperswarm from "hyperswarm";
 
 const ws = new WebSocket("wss://dht2-relay.leet.ar");
 const dht = new DHT(new Stream(true, ws));
-// DHT ...
-const swarm = new Hyperswarm({ dht }); // you pass it as an option
-swarm.on("connection", function (socket, info) {
-  console.log("new swarm connection");
-  socket.on("data", function (data) {
-    console.log("data from peer", b4a.toString(data));
-  });
-
-  // setInterval(() => {
-  //   socket.write('hello: ' + Date.now())
-  // }, 1000)
-  // socket.write('hello')
-  // socket.end()
-});
-
-const topic = b4a.alloc(32).fill("game-room-001");
-swarm.join(topic);
+const swarm = new Hyperswarm({ dht });
 
 function App() {
   const [msg, setMsg] = useState("");
   const [history, setHistory] = useState(["Start of message"])
-  const handlePushMsg = () => {
-    var temp = [...history];
-    temp.push(msg);
+
+  useEffect(() => {
+    console.log('new swarm')
+
+    swarm.on('connection', onsocket)
+
+    const topic = b4a.alloc(32).fill("game-room-002"); // + custom room name, prefix + hash it
+    swarm.join(topic)
+
+    function onsocket (socket, info) {
+      console.log('new peer connected')
+
+      socket.on('error', (err) => console.error('socket error', err.code))
+      socket.on('close', () => console.log('peer disconnected'))
+
+      socket.on('data', function (data) {
+        console.log('new message from peer:', b4a.toString(data));
+        addToHistory(b4a.toString(data));
+      })
+    }
+
+    return () => {
+      swarm.destroy()
+      swarm.off('connection', onsocket)
+    }
+  }, [])
+
+  console.log('new app render')
+
+  const addToHistory = (message) => {
+    const temp = [...history];
+    temp.push(message);
     setHistory(temp)
+  }
+
+  const handlePushMsg = () => {
+    if (!msg) return
+
+    if (swarm.connections.size === 0) {
+      setMsg("");
+      return
+    }
+
+    console.log('sending message to', swarm.connections.size, 'peers:', msg);
+
+    addToHistory(msg);
+
+    for (const socket of swarm.connections) {
+      socket.write(msg);
+    }
+
     setMsg("");
   }
+
   return (
     <div className="App">
       <Container>
