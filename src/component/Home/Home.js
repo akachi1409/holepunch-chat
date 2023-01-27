c296840efa43368da8839e55557923a49429e0c3
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-01-27 04:04:22 -0500

    nav to chat room

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from 'react-router-dom';
 
 function Home () {
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate("/chat")
+    }
     return(
         <>
             <Container>
-                
+                <Row>
+                    <h1>Welcome</h1>
+                </Row>
+                <Row>
+                    <Col lg={8}></Col>
+                    <Col lg={4}>
+                        <Button variant="primary" onClick={() => handleNavigate()}>
+                        Send
+                        </Button>
+                    </Col>
+                </Row>
             </Container>
-            <p>Welcome</p>
+            
         </>
     )
 }
