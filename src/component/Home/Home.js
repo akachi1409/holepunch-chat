da3d05d572a1d3039d8b2395cbb5a65cafd21fd6
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-01-27 03:58:11 -0500

    add router

---
+++ src/component/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Home () {
+    return(
+        <>
+            <Container>
+                
+            </Container>
+            <p>Welcome</p>
+        </>
+    )
+}
+export default Home;
